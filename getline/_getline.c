f7f3c0dd5a3b66dfc9d6476ed39f3a3f19abd603
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 10:23:29 -0600

    1.3 - better comments for variables, validation check

--- getline/_getline.c
+++ getline/_getline.c
@@ -8,9 +8,12 @@
  */
 char *_getline(const int fd)
 {
-    static char buff[READ_SIZE];    /* buffer */
-    static size_t i = 0;            /* current index */
-    static ssize_t buff_bytes = 0;  /* bytes read */
-    char *line = NULL;              /* line to return */
-    size_t line_len = 0;            /* length of line */
+    static char buff[READ_SIZE];    /* buffer to store read data */
+    static size_t i = 0;            /* current index in buffer */
+    static ssize_t buff_bytes = 0;  /* bytes read into buffer */
+    char *line = NULL;              /* string allocated for line */
+    size_t line_len = 0;            /* length of line being read */
+
+if (READ_SIZE <= 0 || fd < 0) /* check for invalid input */
+    return (NULL);
 
