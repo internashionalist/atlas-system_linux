8e196d9e988cbac02a33167d1f56827e52ba30f2
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 07:02:04 -0600

    0.1 - race_state added and it WORKS

--- getline/laps.c
+++ getline/laps.c
@@ -27,3 +27,57 @@ void add_car(int id, int current_count, int *car_ids, int *laps)
     car_ids[i] = id;  /* insert new car */
     laps[i] = 0; /* new car starts with 0 laps */
 }
+
+/**
+ * race_state - tracks lap counts for each car
+ * @id: current update of car IDs
+ * @size: updated number of cars
+ *
+ * Description: This function maintains a static list of cars and
+ * increments lap counts for each car in the update. If new cars are
+ * detected, they are added to the list.
+ * 
+ * Return: void
+ */
+void race_state(int *id, size_t size)
+{
+    static int car_ids[10] = {0}; /* list of cars in race */
+    static int laps[10] = {0};    /* lap counters for each car */
+    static int count = 0;         /* current number of cars */
+    int i, j, found;
+
+    if (size == 0)
+    {
+        return;
+    }
+
+    for (i = 0; i < (int)size; i++)
+    {
+        found = 0;
+
+        /* check if car already in race */
+        for (j = 0; j < count; j++)
+        {
+            if (id[i] == car_ids[j]) /* increment lap count */
+            {
+                laps[j]++;
+                found = 1;
+                break;
+            }
+        }
+
+        /* if car not found, add it */
+        if (!found)
+        {
+            add_car(id[i], count, car_ids, laps);
+            count++;
+        }
+    }
+
+    /* print Race State */
+    printf("Race state:\n");
+    for (j = 0; j < count; j++)
+    {
+        printf("Car %d [%d laps]\n", car_ids[j], laps[j]);
+    }
+}
