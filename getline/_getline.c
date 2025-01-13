433131fd7ac8c8b51dccb688dee7f6e60926a891
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 11:59:30 -0600

    2.1 - integrating task 2 function, still needs work

--- getline/_getline.c
+++ getline/_getline.c
@@ -1,6 +1,32 @@
 #include "_getline.h"
 
 /**
+ * newline_check - checks for \n in string
+ * @buffer: buffer to check
+ * @size: size of buffer
+ * 
+ * Return: updated buffer or NULL if no newline found
+ */
+char *newline_check(char buffer[], int size)
+{
+	int i = 0; /* index for buffer */
+	char *new_buffer = NULL; /* new buffer to store data */
+
+	while (i < size) /* loop through buffer */
+	{
+		if (buffer[i] == '\n') /* check for newline */
+		{
+			new_buffer = malloc(i + 1); /* allocate memory */
+			strncpy(new_buffer, buffer, i); /* copy data */
+			new_buffer[i] = '\0'; /* null-terminate string */
+			return (new_buffer);
+		}
+		i++; /* increment index */
+	}
+	return (NULL); /* no newline */
+}
+
+/**
  * _getline - reads entire line from file descriptor
  * @fd: file descriptor being read
  *
@@ -8,11 +34,12 @@
  */
 char *_getline(const int fd)
 {
-	static char buff[READ_SIZE];   /* buffer to store read data */
-	static size_t i;		   /* current index in buffer */
-	static ssize_t buff_bytes; /* bytes read into buffer */
-	char *line = NULL;			   /* string allocated for line */
-	size_t line_len = 0;		   /* length of line being read */
+	static char buff[READ_SIZE];   	/* buffer to store read data */
+	static size_t i;		   		/* current index in buffer */
+	static ssize_t buff_bytes; 		/* bytes read into buffer */
+	char *line = NULL;			   	/* string allocated for line */
+	size_t line_len = 0;		   	/* length of line being read */
+	char *partial_line;		   		/* partial line from buffer */
 
 	if (READ_SIZE <= 0 || fd < 0) /* check for invalid input */
 		return (NULL);
