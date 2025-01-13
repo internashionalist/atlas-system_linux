8a995af16f980400f22e68f67694db23975b980e
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 07:03:39 -0600

    1.0 - _getline.h

---
+++ getline/_getline.h
@@ -0,0 +1,11 @@
+#ifndef _GETLINE_
+#define _GETLINE_
+#define READ_SIZE 1024
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+char *_getline(const int fd);
+
+#endif
