6542349322953abaa9ccbe7b3882ab769f80ed24
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 12:55:59 -0600

    2.2 - made helper function for partial lines, but I feel like I'm solving the wrong problem?

--- getline/_getline.c
+++ getline/_getline.c
@@ -27,6 +27,37 @@ char *newline_check(char buffer[], int size)
 }
 
 /**
+ * proc_part_line - processes partial line from buffer up to newline
+ * @line: pointer to current line
+ * @line_len: pointer to length of current line
+ * @buff: buffer to read from
+ * @i: pointer to current index in buffer
+ * @buff_bytes: number of bytes read into buffer
+ * 
+ * Return: updated line or NULL on error
+ */
+char *proc_part_line(
+	char *line, size_t *line_len, char buff[], size_t *i, ssize_t buff_bytes)
+{
+	char *partial_line = newline_check(buff + *i, buff_bytes - *i);
+
+	if (partial_line) /* if newline found */
+	{
+		size_t partial_len = strlen(partial_line); /* get length */
+
+		line = realloc(line, *line_len + partial_len + 1); /* reallocate memory */
+		strncpy(line + *line_len, partial_line, partial_len); /* copy data */
+		*line_len += partial_len; /* update length */
+		line[*line_len] = '\0'; /* null-terminate string */
+		free(partial_line); /* free that memory */
+
+		*i += partial_len + 1; /* update index */
+		return (line); /* return updated line */
+	}
+	return (NULL); /* or not */
+}
+
+/**
  * _getline - reads entire line from file descriptor
  * @fd: file descriptor being read
  *
@@ -39,7 +70,6 @@ char *_getline(const int fd)
 	static ssize_t buff_bytes; 		/* bytes read into buffer */
 	char *line = NULL;			   	/* string allocated for line */
 	size_t line_len = 0;		   	/* length of line being read */
-	char *partial_line;		   		/* partial line from buffer */
 
 	if (READ_SIZE <= 0 || fd < 0) /* check for invalid input */
 		return (NULL);
@@ -59,18 +89,8 @@ char *_getline(const int fd)
 				return (line); /* once no more data, return line */
 			}
 		}
-		while (i < (size_t)buff_bytes) /* loop through buffer */
-		{
-			char current_char = buff[i++]; /* iterate current character */
 
-			if (current_char == '\n') /* check for newline */
-			{
-				line = realloc(line, line_len + 1); /* reallocate memory */
-				line[line_len] = '\0'; /* null-terminate line */
-				return (line);		   /* return line */
-			}
-			line = realloc(line, line_len + 1); /* reallocate memory */
-			line[line_len++] = current_char; /* add character to line */
-		}
-	}
-}
+		line = proc_part_line(line, &line_len, buff, &i, buff_bytes);
+		if (line) /* if newline found */
+			return (line); /* that's it */
+
