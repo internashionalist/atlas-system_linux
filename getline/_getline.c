574618e561fed579e58455878345907d9c692b6e
Nash Thames <taylor.thames@atlasschool.com>
2025-01-13 10:17:06 -0600

    1.1 - not making headway, trying with new variables

--- getline/_getline.c
+++ getline/_getline.c
@@ -4,10 +4,13 @@
  * _getline - reads entire line from file descriptor
  * @fd: file descriptor being read
  *
- * Return: text from file descriptor or NULL if none
+ * Return: null-terminated string (excluding \n) or NULL on EOF or error
  */
 char *_getline(const int fd)
 {
-    static char buff[READ_SIZE];  /* buffer */
-    static char *entire = NULL;  /* entire text */
-    
+    static char buff[READ_SIZE];    /* buffer */
+    static size_t i = 0;            /* current index */
+    static ssize_t buff_bytes = 0;  /* bytes read */
+    char *line = NULL;              /* line to return */
+    size_t line_len = 0;            /* length of line */
+
