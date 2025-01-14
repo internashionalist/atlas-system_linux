11b93939a38e19b539c77c4096ee039e97d69949
Nash Thames <taylor.thames@atlasschool.com>
2025-01-14 16:29:12 -0600

    2.4 - bit of formatting, rough struct

--- getline/_getline.h
+++ getline/_getline.h
@@ -1,10 +1,32 @@
-#ifndef _GETLINE_
-#define _GETLINE_
-#define READ_SIZE 1024
+#ifndef _GETLINE_H
+#define _GETLINE_H
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
+
+#ifndef READ_SIZE
+#define READ_SIZE 1024
+#endif
+
+/**
+ * struct file_desc_st - file descriptor state tracker
+ * @fd: file descriptor
+ * @buff: buffer to store read data
+ * @buff_bytes: bytes read into buffer
+ * @i: current index
+ * @next_fd: pointer to next file descriptor state
+ */
+typedef struct file_desc_st
+{
+    int fd;
+    char buff[READ_SIZE];
+    ssize_t buff_bytes;
+    size_t i;
+    struct file_desc_state *next_fd;
+} file_desc_st_t;
 
 char *_getline(const int fd);
 
