cbac8f1578e8f0d72faad36b835f976c3ccd1145
internashionalist <taylor.thames@atlasschool.com>
2025-02-22 04:56:58 -0600

    Makefile / header foundations

---
+++ readelf/include/hreadelf.h
@@ -0,0 +1,12 @@
+#ifndef _HREADELF_H_
+#define _HREADELF_H_
+
+#include <elf.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
