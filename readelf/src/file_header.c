1a3aa4c4c4eb18ee64dce73d2db6eff9574b7e1d
internashionalist <taylor.thames@atlasschool.com>
2025-02-24 03:28:04 -0600

    format unknown

--- readelf/src/file_header.c
+++ readelf/src/file_header.c
@@ -133,7 +133,7 @@ void osabi_(unsigned char osabi)
         case ELFOSABI_TRU64: printf("TRU64 UNIX\n"); break;
         case ELFOSABI_ARM: printf("ARM\n"); break;
         case ELFOSABI_STANDALONE: printf("Standalone\n"); break;
-        default: printf("Unknown\n"); break;
+        default: printf("unknown: %x\n", osabi); break;
     }
 }
 
