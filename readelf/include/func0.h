dff4a95dc855d210b848b6bf1126650b8e1cf8ee
internashionalist <taylor.thames@atlasschool.com>
2025-02-23 19:48:43 -0600

    0.5 - header for Task 0 functions

---
+++ readelf/include/func0.h
@@ -0,0 +1,20 @@
+#ifndef _FUNC0_H_
+#define _FUNC0_H_
+
+#include <elf.h>
+#include <endian.h>
+#include <errno.h>
+#include <error.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* prototypes for Task 0 */
+int print_file_header(elf_info *elf_data)
+
+#endif /* _FUNC0_H_ */
