635592769f09d5b7a6e37d2d38d226301d360145
internashionalist <taylor.thames@atlasschool.com>
2025-02-24 05:09:27 -0600

    betty spaces -> tabs

--- readelf/src/elf_info.c
+++ readelf/src/elf_info.c
@@ -7,13 +7,13 @@
  */
 void magic_(unsigned char *ident)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        printf("%02x ", ident[i]);
-    }
-    printf("\n");
+	for (i = 0; i < 16; i++)
+	{
+		printf("%02x ", ident[i]);
+	}
+	printf("\n");
 }
 
 /**
@@ -22,14 +22,14 @@ void magic_(unsigned char *ident)
  */
 void class_(unsigned char clas)
 {
-    if (clas == ELFCLASS32)
-        printf("ELF32\n");
-    else if (clas == ELFCLASS64)
-        printf("ELF64\n");
-    else if (clas == ELFCLASSNONE)
-        printf("None\n");
-    else
-        printf("<unknown: %x>\n", clas);
+	if (clas == ELFCLASS32)
+		printf("ELF32\n");
+	else if (clas == ELFCLASS64)
+		printf("ELF64\n");
+	else if (clas == ELFCLASSNONE)
+		printf("None\n");
+	else
+		printf("<unknown: %x>\n", clas);
 }
 
 /**
@@ -38,14 +38,14 @@ void class_(unsigned char clas)
  */
 void data_(unsigned char dat)
 {
-    if (dat == ELFDATA2LSB)
-        printf("2's complement, little endian\n");
-    else if (dat == ELFDATA2MSB)
-        printf("2's complement, big endian\n");
-    else if (dat == ELFDATANONE)
-        printf("None\n");
-    else
-        printf("<unknown: %x>\n", dat);
+	if (dat == ELFDATA2LSB)
+		printf("2's complement, little endian\n");
+	else if (dat == ELFDATA2MSB)
+		printf("2's complement, big endian\n");
+	else if (dat == ELFDATANONE)
+		printf("None\n");
+	else
+		printf("<unknown: %x>\n", dat);
 }
 
 /**
@@ -54,12 +54,12 @@ void data_(unsigned char dat)
  */
 void version_(unsigned char version)
 {
-    if (version == EV_CURRENT)
-        printf("%d (current)\n", version);
-    if (version == EV_NONE)
-        printf("None\n");
-    else if (!version)
-        printf("<unknown: %x>\n", version);
+	if (version == EV_CURRENT)
+		printf("%d (current)\n", version);
+	if (version == EV_NONE)
+		printf("None\n");
+	else if (!version)
+		printf("<unknown: %x>\n", version);
 }
 
 /**
@@ -68,36 +68,36 @@ void version_(unsigned char version)
  */
 void osabi_(unsigned char osabi)
 {
-    switch (osabi)
-    {
-    case ELFOSABI_SYSV:
-        printf("UNIX - System V\n");
-        break;
-    case ELFOSABI_HPUX:
-        printf("HP-UX\n");
-        break;
-    case ELFOSABI_NETBSD:
-        printf("UNIX - NetBSD\n");
-        break;
-    case ELFOSABI_LINUX:
-        printf("Linux\n");
-        break;
-    case ELFOSABI_SOLARIS:
-        printf("UNIX - Solaris\n");
-        break;
-    case ELFOSABI_IRIX:
-        printf("IRIX\n");
-        break;
-    case ELFOSABI_FREEBSD:
-        printf("FreeBSD\n");
-        break;
-    case ELFOSABI_TRU64:
-        printf("TRU64 UNIX\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", osabi);
-        break;
-    }
+	switch (osabi)
+	{
+	case ELFOSABI_SYSV:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("UNIX - NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("IRIX\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("FreeBSD\n");
+		break;
+	case ELFOSABI_TRU64:
+		printf("TRU64 UNIX\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", osabi);
+		break;
+	}
 }
 
 /**
@@ -106,27 +106,27 @@ void osabi_(unsigned char osabi)
  */
 void type_(uint16_t type)
 {
-    switch (type)
-    {
-    case ET_NONE:
-        printf("None\n");
-        break;
-    case ET_REL:
-        printf("REL (Relocatable file)\n");
-        break;
-    case ET_EXEC:
-        printf("EXEC (Executable file)\n");
-        break;
-    case ET_DYN:
-        printf("DYN (Shared object file)\n");
-        break;
-    case ET_CORE:
-        printf("Core file\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", type);
-        break;
-    }
+	switch (type)
+	{
+	case ET_NONE:
+		printf("None\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("Core file\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", type);
+		break;
+	}
 }
 
 /**
@@ -135,22 +135,22 @@ void type_(uint16_t type)
  */
 void machine_(uint16_t machine)
 {
-    switch (machine)
-    {
-    case EM_NONE:
-        printf("None\n");
-        break;
-    case EM_SPARC:
-        printf("Sparc\n");
-        break;
-    case EM_386:
-        printf("Intel 80386\n");
-        break;
-    case EM_X86_64:
-        printf("Advanced Micro Devices X86-64\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", machine);
-        break;
-    }
+	switch (machine)
+	{
+	case EM_NONE:
+		printf("None\n");
+		break;
+	case EM_SPARC:
+		printf("Sparc\n");
+		break;
+	case EM_386:
+		printf("Intel 80386\n");
+		break;
+	case EM_X86_64:
+		printf("Advanced Micro Devices X86-64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", machine);
+		break;
+	}
 }
