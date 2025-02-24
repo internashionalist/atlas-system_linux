bc669f6107a7aac4904fbb3f303bdd2d6fec4150
internashionalist <taylor.thames@atlasschool.com>
2025-02-23 19:38:10 -0600

    0.4 - header print function

---
+++ readelf/src/func0.c
@@ -0,0 +1,38 @@
+#include "hreadelf.h"
+
+/**
+ * print_file_header - prints the ELF file header
+ * @elf_data: ELF file data structure
+ *
+ * Return: 1 on success, 0 if invalid data
+ */
+int print_file_header(elf_info *elf_data)
+{
+    if (!elf_data) /* if NULL or invalid data */
+    {
+        return (0);
+    }
+
+    printf("ELF Header:\n"); /* print header */
+
+    if (prog.file_class == ELFCLASS32) /* 32-bit ELF */
+    {
+        if (prog.endianness == LITTLE_ENDIAN)
+        {
+            adjust_fh_endian_32(&elf_data->hdr32);
+        }
+
+        display_hdr_details_32(&elf_data->hdr32);
+    }
+    else if (prog.file_class == ELFCLASS64) /* 64-bit ELF */
+    {
+        if (prog.endianness == BIG_ENDIAN)
+        {
+            adjust_fh_endian_64(&elf_data->hdr64);
+        }
+
+        display_hdr_details_64(&elf_data->hdr64);
+    }
+
+    return (1);
+}
