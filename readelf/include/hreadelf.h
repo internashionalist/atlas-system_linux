057c8378f49a5bee4daca0a2e4c7a5ca0aedd16e
internashionalist <taylor.thames@atlasschool.com>
2025-02-23 19:23:39 -0600

    0.3 - beautiful header if I do say so

--- readelf/include/hreadelf.h
+++ readelf/include/hreadelf.h
@@ -8,5 +8,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
+/* endianness */
+#define LITTLE_ENDIAN 1
+#define BIG_ENDIAN 2
+
+/* error messages */
+#define ERROR_MSG ": Error: "
+#define FILE_NOT_FOUND_MSG ": File not found"
+#define INVALID_ELF_MSG "Invalid ELF file - magic bytes mismatch"
+
+/* ELF header identifiers */
+#define HDR_MAGIC " Magic:  "
+#define HDR_CLASS "Class:"
+#define HDR_DATA "Data:"
+#define HDR_VERSION "Version:"
+#define HDR_OSABI "OS/ABI:"
+#define HDR_ABI_VER "ABI Version:"
+#define HDR_TYPE "Type:"
+#define HDR_MACHINE "Machine:"
+#define HDR_FILE_VER "File Version:"
+#define HDR_ENT_PT "Entry Point:"
+#define HDR_FLAGS "Flags:"
+#define HDR_SIZE "Header Size:"
+
+/**
+ * struct program_info - contains information about the program
+ * @program_name:   program name
+ * @input_name:     input ELF file name
+ * @file_desc:      file descriptor for the ELF file
+ * @file_class:     file class (1 for 32-bit, 2 for 64-bit)
+ * @endianness:     endianness of the ELF file (1 for little, 2 for big)
+ */
+typedef struct program_info
+{
+    char *program_name; /* program name */
+    char *input_name;   /* input ELF file name */
+    int file_desc;      /* ELF file descriptor */
+    int file_class;     /* 1 for 32-bit, 2 for 64-bit */
+    int endianness;     /* 1 for little, 2 for big */
+} prog_info;
+
+/**
+ * elf_info - holds the parsed ELF header data
+ * @hdr32:   32-bit ELF header structure
+ * @hdr64:   64-bit ELF header structure
+ */
+typedef struct elf_info
+{
+    Elf32_Ehdr hdr32;   /* 32-bit ELF header */
+    Elf64_Ehdr hdr64;   /* 64-bit ELF header */
+} elf_info;
+
+/* reference to program_info */
+extern prog_info prog;
+
+/* header details extraction */
+void display_hdr_details_32(Elf32_Ehdr *hdr32);
+void display_hdr_details_64(Elf64_Ehdr *hdr64);
+
+#endif /* _HREADELF_H_ */
