3d5d0cbd6c9942d7f2b3e6aa67c6c1904484b3c6
internashionalist <taylor.thames@atlasschool.com>
2025-02-24 05:14:25 -0600

    betty

--- readelf/src/elf_info2.c
+++ readelf/src/elf_info2.c
@@ -7,36 +7,36 @@
  */
 void osabi_(unsigned char osabi)
 {
-    switch (osabi)
-    {
-    case ELFOSABI_SYSV:
-        printf("UNIX - System V\n");
-        break;
-    case ELFOSABI_HPUX:
-        printf("HP-UX\n");
-        break;
-    case ELFOSABI_NETBSD:
-        printf("UNIX - NetBSD\n");
-        break;
-    case ELFOSABI_LINUX:
-        printf("Linux\n");
-        break;
-    case ELFOSABI_SOLARIS:
-        printf("UNIX - Solaris\n");
-        break;
-    case ELFOSABI_IRIX:
-        printf("IRIX\n");
-        break;
-    case ELFOSABI_FREEBSD:
-        printf("FreeBSD\n");
-        break;
-    case ELFOSABI_TRU64:
-        printf("TRU64 UNIX\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", osabi);
-        break;
-    }
+	switch (osabi)
+	{
+	case ELFOSABI_SYSV:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("UNIX - NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("IRIX\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("FreeBSD\n");
+		break;
+	case ELFOSABI_TRU64:
+		printf("TRU64 UNIX\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", osabi);
+		break;
+	}
 }
 
 /**
@@ -45,27 +45,27 @@ void osabi_(unsigned char osabi)
  */
 void type_(uint16_t type)
 {
-    switch (type)
-    {
-    case ET_NONE:
-        printf("None\n");
-        break;
-    case ET_REL:
-        printf("REL (Relocatable file)\n");
-        break;
-    case ET_EXEC:
-        printf("EXEC (Executable file)\n");
-        break;
-    case ET_DYN:
-        printf("DYN (Shared object file)\n");
-        break;
-    case ET_CORE:
-        printf("Core file\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", type);
-        break;
-    }
+	switch (type)
+	{
+	case ET_NONE:
+		printf("None\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("Core file\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", type);
+		break;
+	}
 }
 
 /**
@@ -74,22 +74,22 @@ void type_(uint16_t type)
  */
 void machine_(uint16_t machine)
 {
-    switch (machine)
-    {
-    case EM_NONE:
-        printf("None\n");
-        break;
-    case EM_SPARC:
-        printf("Sparc\n");
-        break;
-    case EM_386:
-        printf("Intel 80386\n");
-        break;
-    case EM_X86_64:
-        printf("Advanced Micro Devices X86-64\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", machine);
-        break;
-    }
+	switch (machine)
+	{
+	case EM_NONE:
+		printf("None\n");
+		break;
+	case EM_SPARC:
+		printf("Sparc\n");
+		break;
+	case EM_386:
+		printf("Intel 80386\n");
+		break;
+	case EM_X86_64:
+		printf("Advanced Micro Devices X86-64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", machine);
+		break;
+	}
 }
--- readelf/src/endianness.c
+++ readelf/src/endianness.c
@@ -9,23 +9,23 @@
  */
 void adjust_fh_endian_32(Elf32_Ehdr *hdr32)
 {
-    if (prog.endianness != LSB)
-    {
-        hdr32->e_type = byte_swap16(hdr32->e_type);
-        hdr32->e_machine = byte_swap16(hdr32->e_machine);
-        hdr32->e_version = byte_swap32(hdr32->e_version);
-        hdr32->e_entry = byte_swap32(hdr32->e_entry);
-        hdr32->e_phoff = byte_swap32(hdr32->e_phoff);
-        hdr32->e_shoff = byte_swap32(hdr32->e_shoff);
-        hdr32->e_flags = byte_swap32(hdr32->e_flags);
-    }
+	if (prog.endianness != LSB)
+	{
+		hdr32->e_type = byte_swap16(hdr32->e_type);
+		hdr32->e_machine = byte_swap16(hdr32->e_machine);
+		hdr32->e_version = byte_swap32(hdr32->e_version);
+		hdr32->e_entry = byte_swap32(hdr32->e_entry);
+		hdr32->e_phoff = byte_swap32(hdr32->e_phoff);
+		hdr32->e_shoff = byte_swap32(hdr32->e_shoff);
+		hdr32->e_flags = byte_swap32(hdr32->e_flags);
+	}
 
-    hdr32->e_ehsize = byte_swap16(hdr32->e_ehsize);
-    hdr32->e_phentsize = byte_swap16(hdr32->e_phentsize);
-    hdr32->e_phnum = byte_swap16(hdr32->e_phnum);
-    hdr32->e_shentsize = byte_swap16(hdr32->e_shentsize);
-    hdr32->e_shnum = byte_swap16(hdr32->e_shnum);
-    hdr32->e_shstrndx = byte_swap16(hdr32->e_shstrndx);
+	hdr32->e_ehsize = byte_swap16(hdr32->e_ehsize);
+	hdr32->e_phentsize = byte_swap16(hdr32->e_phentsize);
+	hdr32->e_phnum = byte_swap16(hdr32->e_phnum);
+	hdr32->e_shentsize = byte_swap16(hdr32->e_shentsize);
+	hdr32->e_shnum = byte_swap16(hdr32->e_shnum);
+	hdr32->e_shstrndx = byte_swap16(hdr32->e_shstrndx);
 }
 
 /**
@@ -36,21 +36,21 @@ void adjust_fh_endian_32(Elf32_Ehdr *hdr32)
  */
 void adjust_fh_endian_64(Elf64_Ehdr *hdr64)
 {
-    if (prog.endianness != MSB)
-    {
-        hdr64->e_type = byte_swap16(hdr64->e_type);
-        hdr64->e_machine = byte_swap16(hdr64->e_machine);
-        hdr64->e_version = byte_swap32(hdr64->e_version);
-        hdr64->e_entry = byte_swap64(hdr64->e_entry);
-        hdr64->e_phoff = byte_swap64(hdr64->e_phoff);
-        hdr64->e_shoff = byte_swap64(hdr64->e_shoff);
-        hdr64->e_flags = byte_swap32(hdr64->e_flags);
-    }
+	if (prog.endianness != MSB)
+	{
+		hdr64->e_type = byte_swap16(hdr64->e_type);
+		hdr64->e_machine = byte_swap16(hdr64->e_machine);
+		hdr64->e_version = byte_swap32(hdr64->e_version);
+		hdr64->e_entry = byte_swap64(hdr64->e_entry);
+		hdr64->e_phoff = byte_swap64(hdr64->e_phoff);
+		hdr64->e_shoff = byte_swap64(hdr64->e_shoff);
+		hdr64->e_flags = byte_swap32(hdr64->e_flags);
+	}
 
-    hdr64->e_ehsize = byte_swap16(hdr64->e_ehsize);
-    hdr64->e_phentsize = byte_swap16(hdr64->e_phentsize);
-    hdr64->e_phnum = byte_swap16(hdr64->e_phnum);
-    hdr64->e_shentsize = byte_swap16(hdr64->e_shentsize);
-    hdr64->e_shnum = byte_swap16(hdr64->e_shnum);
-    hdr64->e_shstrndx = byte_swap16(hdr64->e_shstrndx);
+	hdr64->e_ehsize = byte_swap16(hdr64->e_ehsize);
+	hdr64->e_phentsize = byte_swap16(hdr64->e_phentsize);
+	hdr64->e_phnum = byte_swap16(hdr64->e_phnum);
+	hdr64->e_shentsize = byte_swap16(hdr64->e_shentsize);
+	hdr64->e_shnum = byte_swap16(hdr64->e_shnum);
+	hdr64->e_shstrndx = byte_swap16(hdr64->e_shstrndx);
 }
