e8380ed5c5977fa4b8a6a996dbf674b26dbb0043
internashionalist <taylor.thames@atlasschool.com>
2025-02-24 02:28:18 -0600

    format printing

--- readelf/src/file_header.c
+++ readelf/src/file_header.c
@@ -7,7 +7,7 @@
  */
 void display_hdr_details_32(Elf32_Ehdr *map_32)
 {
-    printf("%s", HDR_MAGIC); magic_(map_32->e_ident);
+    printf(HDR_MAGIC); magic_(map_32->e_ident);
     printf("  %-35s", HDR_CLASS); class_(map_32->e_ident[EI_CLASS]);
     printf("  %-35s", HDR_DATA); data_(map_32->e_ident[EI_DATA]);
     printf("  %-35s", HDR_VERSION); version_(map_32->e_ident[EI_VERSION]);
@@ -15,7 +15,7 @@ void display_hdr_details_32(Elf32_Ehdr *map_32)
     printf("  %-35s%d\n", HDR_ABI_VER, map_32->e_ident[EI_ABIVERSION]);
     printf("  %-35s", HDR_TYPE); type_(map_32->e_type);
     printf("  %-35s", HDR_MACHINE); machine_(map_32->e_machine);
-    printf("  %-35s0x%x\n", HDR_FILE_VER, map_32->e_version);
+    printf("  %-35s%#x\n", HDR_FILE_VER, map_32->e_version);
     printf("  %-35s0x%x\n", HDR_ENT_PT, map_32->e_entry);
     printf("  %-35s0x%x\n", HDR_FLAGS, map_32->e_flags);
     printf("  %-35s%d (bytes)\n", HDR_SIZE, map_32->e_ehsize);
@@ -32,7 +32,7 @@ void display_hdr_details_32(Elf32_Ehdr *map_32)
  */
 void display_hdr_details_64(Elf64_Ehdr *map_64)
 {
-    printf("%s", HDR_MAGIC), magic_(map_64->e_ident);
+    printf(HDR_MAGIC), magic_(map_64->e_ident);
     printf("  %-35s", HDR_CLASS), class_(map_64->e_ident[EI_CLASS]);
     printf("  %-35s", HDR_DATA), data_(map_64->e_ident[EI_DATA]);
     printf("  %-35s", HDR_VERSION), version_(map_64->e_ident[EI_VERSION]);
@@ -40,7 +40,7 @@ void display_hdr_details_64(Elf64_Ehdr *map_64)
     printf("  %-35s%d\n", HDR_ABI_VER, map_64->e_ident[EI_ABIVERSION]);
     printf("  %-35s", HDR_TYPE), type_(map_64->e_type);
     printf("  %-35s", HDR_MACHINE), machine_(map_64->e_machine);
-    printf("  %-35s0x%x\n", HDR_FILE_VER, map_64->e_version);
+    printf("  %-35s%#x\n", HDR_FILE_VER, map_64->e_version);
     printf("  %-35s0x%lx\n", HDR_ENT_PT, map_64->e_entry);
     printf("  %-35s0x%x\n", HDR_FLAGS, map_64->e_flags);
     printf("  %-35s%d (bytes)\n", HDR_SIZE, map_64->e_ehsize);
@@ -59,10 +59,9 @@ void magic_(unsigned char *ident)
 {
     int i;
 
-    printf("  Magic:   ");
     for (i = 0; i < 16; i++)
     {
-        printf("%02x ", ident[i]);
+        printf("%.2x \n", ident[i]);
     }
     printf("\n");
 }
