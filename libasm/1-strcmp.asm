bf945ddaf761c2b044a3348ce0f62b65f57131b9
Nash Thames <taylor.thames@atlasschool.com>
2025-03-02 02:34:44 +0000

    adjusted to read bytes directly

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -12,33 +12,33 @@ section .text
 ; Prototype:			int asm_strcmp(const char *str1, const char *str2);
 
 asm_strcmp:
-.loop:					; compares str1 and str2
-	mov al, [rdi]		; load byte from str1 into al
-	mov bl, [rsi]		; load byte from str2 into bl
-	cmp al, bl			; compare bytes
-	jne .different		; if different, jump to .different
-	cmp al, 0			; check if null terminator
-	je .equal			; if yes, jump to .equal
-	inc rdi				; increment str1 pointers
-	inc rsi				; increment str2 pointers
-	jmp .loop			; rinse, repeat
+.loop:						; compares str1 and str2
+	mov 	al, [rdi]		; load byte from str1 into al
+	mov 	bl, [rsi]		; load byte from str2 into bl
+	cmp 	al, bl			; compare bytes
+	jne 	.different		; if different, jump to .different
+	cmp 	al, 0			; check if null terminator
+	je 		.equal			; if yes, jump to .equal
+	inc 	rdi				; increment str1 pointers
+	inc 	rsi				; increment str2 pointers
+	jmp 	.loop			; rinse, repeat
 
 .different:
-	movzx eax, al		; zero-extend byte from str1 into eax
-	movzx ebx, bl		; zero-extend byte from str2 into ebx
-	sub eax, ebx		; compute difference (str1 - str2)
-	cmp eax, 0			; check if difference is 0
-	jg .positive		; if positive, jump to .positive
-	jl .negative		; if negative, jump to .negative
+	movzx 	eax, byte [rdi]	; zero-extend byte from str1 into eax
+	movzx 	ebx, byte [rsi]	; zero-extend byte from str2 into ebx
+	sub 	eax, ebx		; compute difference (str1 - str2)
+	cmp 	eax, 0			; check if difference is 0
+	jg 		.positive		; if positive, jump to .positive
+	jl 		.negative		; if negative, jump to .negative
 
 .positive:
-	mov eax, 1			; set eax to 1 (str1 > str2)
-	ret					; return 1
+	mov 	eax, 1			; set eax to 1 (str1 > str2)
+	ret						; return 1
 
 .negative:
-	mov eax, -1		; set eax to -1 (str1 < str2)
-	ret					; return -1
+	mov 	eax, -1			; set eax to -1 (str1 < str2)
+	ret						; return -1
 
 .equal:
-	xor eax, eax		; set eax to 0 (str1 == str2)
-	ret					; return 0
+	xor 	eax, eax		; set eax to 0 (str1 == str2)
+	ret						; return 0
