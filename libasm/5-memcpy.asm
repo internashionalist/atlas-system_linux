9dcebfe4ec0f2d4e15bc590f5e9784ff169639ae
Nash Thames <taylor.thames@atlasschool.com>
2025-03-02 08:46:34 +0000

    getting the hang of this! memcpy runs.

---
+++ libasm/5-memcpy.asm
@@ -0,0 +1,30 @@
+BITS 64
+
+global asm_memcpy
+
+section .text
+
+; asm_memcpy: copies n bytes from memory area src => dest - memcpy(3)
+; @dest:		rdi
+; @src:			rsi
+; @n:			rdx
+;
+; Return:		rax (start of dest)
+;
+; Prototype:	void *asm_memcpy(void *dest, const void *src, size_t n);
+
+asm_memcpy:
+	mov 			rax, rdi		; save dest in rax
+	test 			rdx, rdx		; test if n == 0 (no bytes to copy)
+	je 				.fin			; if n == 0, jump to .fin
+
+.copy_loop:
+	mov 			bl, [rsi]		; load byte from src to bl
+	mov 			[rdi], bl		; store byte from bl to dest
+	inc 			rsi				; move to next byte in src
+	inc 			rdi				; move to next byte in dest
+	dec 			rdx				; n--
+	jnz 			.copy_loop		; continue until n == 0
+
+.fin:
+	ret								; rax set to original dest already
