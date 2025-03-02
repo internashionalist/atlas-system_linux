cb71d8c7dbfdf5dc108c3c07f3999e0e42c78de1
Nash Thames <taylor.thames@atlasschool.com>
2025-03-02 09:03:07 +0000

    return void or bytes written / -1 ???

---
+++ libasm/6-putc.asm
@@ -0,0 +1,26 @@
+BITS 64
+
+global asm_putc
+
+section .text
+
+; asm_putc: 	writes a character to stdout - putc(3)
+; @c:			rdi (character to write)
+;
+; Return:		number of bytes written or -1 on error
+;
+; Prototype:	void asm_putc(int c);
+
+asm_putc:
+	sub			rsp, 8			; align stack
+	mov			[rsp], dil		; save c to stack (lower 8 bits)
+
+	mov		eax, 1			; syscall number for sys_write
+	mov		edi, 1			; file descriptor for stdout
+	mov		rsi, rsp		; pointer to c (on the stack)
+	mov		rdx, 1			; length of c (1 byte)
+
+	syscall					; write character to stdout
+
+	add			rsp, 8		; restore stack pointer
+	ret						; bytes written or -1 on error
