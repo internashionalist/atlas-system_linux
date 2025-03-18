b8ce9e36ed36c22c64f29d4f625c97f58162cb7c
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 18:46:14 +0000

    maybe put it in the header file??

--- signals/signals.h
+++ signals/signals.h
@@ -8,6 +8,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * struct sigaction -   structure for signal handling
+ * @sa_handler:         pointer to the signal handling function
+ * @sa_flags:           flags that modify the behavior of the signal
+ * @sa_mask:            set of signals to be blocked during execution
+ *                      of the handler
+ *
+ * Description: used by the sigaction system call to specify how a signal
+ *              should be handled.
+ */
+
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
