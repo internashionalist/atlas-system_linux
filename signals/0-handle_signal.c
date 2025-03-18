25489c9f2a90bba521f245d83535fc9d48156e1d
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 07:00:16 +0000

    betty

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -5,10 +5,10 @@
 #include "signals.h"
 
 /**
- * gotcha_printer - 	prints "Gotcha!" on receiving a signal
+ * gotcha_printer -		prints "Gotcha!" on receiving a signal
  *						(plus signal number)
- * @signum: 			signal number to print
- * 
+ * @signum:				signal number to print
+ *
  * Return:				void
  */
 void gotcha_printer(int signum)
@@ -19,7 +19,7 @@ void gotcha_printer(int signum)
 
 /**
  * handle_signal -		handles SIGINT signal when received
- * 
+ *
  * Return:				0 on success, -1 on failure
  */
 int handle_signal(void)
