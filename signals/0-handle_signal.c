806f585d1bc350eb5b411e0af25f8f632d92110a
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 06:59:23 +0000

    GOTCHA

---
+++ signals/0-handle_signal.c
@@ -0,0 +1,32 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "signals.h"
+
+/**
+ * gotcha_printer - 	prints "Gotcha!" on receiving a signal
+ *						(plus signal number)
+ * @signum: 			signal number to print
+ * 
+ * Return:				void
+ */
+void gotcha_printer(int signum)
+{
+	fprintf(stdout, "Gotcha! [%d]\n", signum);
+	fflush(stdout);
+}
+
+/**
+ * handle_signal -		handles SIGINT signal when received
+ * 
+ * Return:				0 on success, -1 on failure
+ */
+int handle_signal(void)
+{
+	if (signal(SIGINT, gotcha_printer) == SIG_ERR)
+	{
+		return (-1);
+	}
+	return (0);
+}
---
+++ signals/signals.h
@@ -0,0 +1,18 @@
+#ifndef _SIGNALS_H_
+#define _SIGNALS_H_
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+
+#endif
