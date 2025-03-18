f52ae3fae8767e72c00f5500507797b830dfff13
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 14:07:50 +0000

    betty

--- signals/2-handle_sigaction.c
+++ signals/2-handle_sigaction.c
@@ -13,8 +13,8 @@
  */
 void gotcha_printer(int signum)
 {
-    fprintf(stdout, "Gotcha! [%d]\n", signum);
-    fflush(stdout);
+	fprintf(stdout, "Gotcha! [%d]\n", signum);
+	fflush(stdout);
 }
 
 /**
@@ -24,15 +24,15 @@ void gotcha_printer(int signum)
  */
 int handle_sigaction(void)
 {
-    struct sigaction sa;  /* struct to handle signal actions */
+	struct sigaction sa;  /* struct to handle signal actions */
 
-    memset(&sa, 0, sizeof(sa));  /* initialize struct to 0 */
-    sa.sa_handler = gotcha_printer;  /* set handler to gotcha_printer */
+	memset(&sa, 0, sizeof(sa));  /* initialize struct to 0 */
+	sa.sa_handler = gotcha_printer;  /* set handler to gotcha_printer */
 
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-    {
-        return (-1);
-    }
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+		return (-1);
+	}
 
-    return (0);
+	return (0);
 }
