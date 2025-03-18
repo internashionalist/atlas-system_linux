ecc9af6f6afaa3024cfee830b3e6581d496d5956
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 18:51:34 +0000

    damn it betty

--- signals/signals.h
+++ signals/signals.h
@@ -9,14 +9,14 @@
 #include <unistd.h>
 
 /**
- * struct sigaction -   structure for signal handling
- * @sa_handler:         pointer to the signal handling function
- * @sa_flags:           flags that modify the behavior of the signal
- * @sa_mask:            set of signals to be blocked during execution
- *                      of the handler
+ * sigaction_t -	structure for signal handling
+ * @sa_handler:		pointer to the signal handling function
+ * @sa_flags:		flags that modify the behavior of the signal
+ * @sa_mask:		set of signals to be blocked during execution
+ *					of the handler
  *
- * Description: used by the sigaction system call to specify how a signal
- *              should be handled.
+ * Description: 	used by the sigaction system call to specify how
+ *					a signal should be handled
  */
 typedef struct sigaction sigaction_t;
 
