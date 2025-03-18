af4d9d920aca779500d74587f16f28913919c5eb
Nash Thames <taylor.thames@atlasschool.com>
2025-03-18 23:25:23 +0000

    strsignal FTW

---
+++ signals/5-signal_describe.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "signals.h"
+
+/**
+ * main - puts out a description for a given signal
+ * @argc: argument count (as integer)
+ * @argv: argument vector (as string)
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)  /* argument count check */
+	{
+		fprintf(stderr, "Usage: %s <signum>\n", argv[0]);  /* print usage */
+		return (EXIT_FAILURE);
+	}
+
+	int signum = atoi(argv[1]);  /* convert arg string to int */
+	printf("%s\n", strsignal(signum));  /* print signal description */
+
+	return (EXIT_SUCCESS);
+}
