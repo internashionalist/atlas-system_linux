b182cf1b15195d5a56c56eeea893697e1464a8c5
Nash Thames <taylor.thames@atlasschool.com>
2025-03-19 00:25:42 +0000

    learning lots about

---
+++ signals/6-suspend.c
@@ -0,0 +1,36 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "signals.h"
+
+/**
+ * caught_printer - handles SIGINT signals, prints a message
+ * @signum:			signal number caught
+ *
+ * Return:			void
+ */
+void caught_printer(int signum)
+{
+	printf("Caught %d\n", signum);
+}
+
+/**
+ * main - sets SIGINT handler and waits for signal
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error
+ */
+int main(void)
+{
+	sigaction_t sa;						/* sigaction struct */
+
+	sa.sa_handler = caught_printer;		/* set handler */
+	sa.sa_flags = 0;					/* set flags */
+	sigemptyset(&sa.sa_mask);			/* set mask */
+	sigaction(SIGINT, &sa, NULL);		/* update action */
+	pause();							/* wait for signal */
+
+	printf("Signal received\n");		/* print message on signal */	
+
+	return (EXIT_SUCCESS);
+}
