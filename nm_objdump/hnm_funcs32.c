caeaaf8a86b7e35c11c9039922b0fef0f12d551d
Nash Thames <taylor.thames@atlasschool.com>
2025-03-26 10:08:05 +0000

    32bit match

--- nm_objdump/hnm_funcs32.c
+++ nm_objdump/hnm_funcs32.c
@@ -71,6 +71,8 @@ static char get_symbol_char32(Elf32_Sym *sym, Elf32_Shdr *sections,
 		return ('U');
 	if (sym->st_shndx == SHN_ABS)
 		return ((bind == STB_LOCAL) ? 'a' : 'A');
+	if (type == STT_FUNC)
+		return ((bind == STB_LOCAL) ? 't' : 'T');
 	sec = &sections[sym->st_shndx];
 	if (sec->sh_type == SHT_NOBITS)
 		return ((bind == STB_LOCAL) ? 'b' : 'B');
