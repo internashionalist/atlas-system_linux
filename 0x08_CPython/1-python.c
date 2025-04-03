6ec181c55164d6471fd47f03fa6130f099916f27
Nash Thames <taylor.thames@atlasschool.com>
2025-04-02 19:41:52 -0500

    good comments = good time

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -14,21 +14,21 @@ void print_python_list(PyObject *p)
 	PyListObject *list;				/* PyListObject struct */
 
 	printf("[*] Python list info\n");
-	if (!PyList_Check(p))
+	if (!PyList_Check(p))           /* check if p is list */
 	{
 		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
 
-	size = PyList_Size(p);
-	list = (PyListObject *)p;
+	size = PyList_Size(p);          /* get size of list */
+	list = (PyListObject *)p;       /* cast PyObject to PyListObject */
 
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)      /* iterate through list */
 	{
-		PyObject *item = PyList_GetItem(p, i);
+		PyObject *item = PyList_GetItem(p, i);  /* get item at i */
 
 		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
 	}
