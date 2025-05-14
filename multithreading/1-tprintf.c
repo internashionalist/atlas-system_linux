08edd507d8ec15ee6ec9d3aabfc11b2c7d1359c5
Nash Thames <taylor.thames@atlasschool.com>
2025-05-14 04:35:12 +0000

    1.0 - Betty likes it now

---
+++ multithreading/1-tprintf.c
@@ -0,0 +1,23 @@
+#include "multithreading.h"
+
+/**
+ * tprintf - uses the printf family to print out a given formatted string
+ * @format: format string
+ *
+ * This function is a wrapper around the printf function
+ * that prints the formatted string to stdout.
+ * 
+ * Return: the number of characters printed
+ */
+int tprintf(const char *format, ...)
+{
+	va_list args;
+	int ret;
+
+	printf("[%ld] ", pthread_self());
+
+	va_start(args, format);
+	ret = vprintf(format, args);
+	va_end(args);
+	return (ret);
+}
